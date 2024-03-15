48418cb790a74c0386de0df5557df5d023be62a7
Jay Shitre <jayshitre@gmail.com>
2024-03-15 14:36:34 +0530

    Fixed text sizes in mobile view of Landing Section

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -13,23 +13,23 @@ const LandingSection = () => {
 						<h1 className={"text-white text-center md:text-left text-5xl lg:text-6xl xl:text-7xl font-serif w-full"}>Discovery.</h1>
 					</div>
 
-					<div className={"flex items-center flex-col gap-4"}>
+					<div className={"flex items-center flex-col gap-0 md:gap-4"}>
 						<div className={"flex text-white gap-0 lg:gap-4 text-xl font-medium "}>
-							<div className={"flex flex-col items-center w-[125px]"}>
-								<h1 className={"text-center"}>Experimental </h1>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
+								<h1 className={"text-center "}>Experimental </h1>
 								<h1 className={"text-center"}>Data</h1>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
 								<IoIosArrowRoundDown className={"block md:hidden text-white"} size={70} />
 							</div>
 
-							<div className={"flex flex-col items-center w-[125px]"}>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
 								<h1 className={"text-center"}>Simulation </h1>
 								<h1 className={"text-center"}>Data</h1>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
 								<IoIosArrowRoundDown className={"block md:hidden text-white"} size={70} />
 							</div>
 
-							<div className={"flex flex-col items-center w-[125px]"}>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
 								<h1 className={"text-center"}>Physical </h1>
 								<h1 className={"text-center"}>Laws</h1>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
@@ -37,32 +37,32 @@ const LandingSection = () => {
 							</div>
 						</div>
 
-						<div className={"flex flex-col text-white gap-2"}>
-							<h1 className={"font-medium text-center text-xl font-semibold"}>Inductive Priors</h1>
+						<div className={"flex flex-col text-white gap-0 md:gap-2"}>
+							<h1 className={"font-medium text-center text-base md:text-xl font-semibold"}>Inductive Priors</h1>
 							<div className={"flex gap-2"}>
-								<div className={"flex flex-col gap-2"}>
-								<h1 className={"font-serif text-center text-3xl md:text-4xl"}>Scientific Foundation</h1>
-								<h1 className={"font-serif text-center text-3xl md:text-4xl"}>Models.</h1>
+								<div className={"flex flex-col gap-0 md:gap-2"}>
+									<h1 className={"font-serif text-center text-2xl md:text-4xl"}>Scientific Foundation</h1>
+									<h1 className={"font-serif text-center text-2xl md:text-4xl"}>Models.</h1>
 								</div>
 							</div>
 						</div>
 
 						<div className={"flex text-white gap-0 lg:gap-4 text-xl font-medium"}>
-							<div className={"flex flex-col items-center w-[125px]"}>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
 								<IoIosArrowRoundDown className={"block md:hidden text-white"} size={70} />
 								<h1 className={"text-center"}>New </h1>
 								<h1 className={"text-center"}>Medicine</h1>
 							</div>
 
-							<div className={"flex flex-col items-center w-[125px]"}>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
 								<IoIosArrowRoundDown className={"block md:hidden text-white"} size={70} />
 								<h1 className={"text-center"}>Novel </h1>
 								<h1 className={"text-center"}>Alloys</h1>
 							</div>
 
-							<div className={"flex flex-col items-center w-[125px]"}>
+							<div className={"flex flex-col items-center w-[100px] md:w-[125px] text-base md:text-xl"}>
 								<IoIosArrowRoundDown className={"hidden md:block text-white"} size={100} />
 								<IoIosArrowRoundDown className={"block md:hidden text-white"} size={70} />
 								<h1 className={"text-center"}>Fundamental</h1>
