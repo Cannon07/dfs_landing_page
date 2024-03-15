699b4cc3cbaa8a354357445ae95397c180ba2a83
Jay Shitre <jayshitre@gmail.com>
2024-03-16 05:12:38 +0530

    Fixed Logo Carosuel for mobile

--- src/components/SponserSection.tsx
+++ src/components/SponserSection.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
 const SponserSection = () => {
+	const logo_height: number = 150;
+	const logo_width: number = 150;
+
+	const logo_height_mobile: number = 100;
+	const logo_width_mobile: number = 100;
+
 	return (
 		<div className={"bg-white flex flex-col gap-8 py-10"}>
 			<div className={"w-full flex justify-center"}>
@@ -13,43 +19,44 @@ const SponserSection = () => {
 					</div>
 				</div>
 			</div>
+			<div className={"hidden md:block"}>
 			<div className={"flex space-x-8 md:space-x-16 overflow-hidden"}>
 				<div className={"flex space-x-8 md:space-x-16 animate-loop-scroll"}>
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/australian_national_university.svg"}
 						alt={"australian_national_university Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/aws.svg"}
 						alt={"AWS Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/bayer.svg"}
 						alt={"bayer Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/broad_inst.svg"}
 						alt={"broad_inst Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/carnegie_mellon_university.svg"}
 						alt={"carnegie_mellon_university Icon"}
 						className={"max-w-none"}
@@ -57,66 +64,66 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/download.png"}
 						alt={"Chinese Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/expedia.svg"}
 						alt={"expedia Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/firmenich.svg"}
 						alt={"firmenich Icon"}
 						className={"max-w-none"}
 					/>
 				</div>
 
-				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll group-hover:paused"} aria-hidden="true">
+				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll"} aria-hidden="true">
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/australian_national_university.svg"}
 						alt={"australian_national_university Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/aws.svg"}
 						alt={"AWS Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/bayer.svg"}
 						alt={"bayer Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/broad_inst.svg"}
 						alt={"broad_inst Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/carnegie_mellon_university.svg"}
 						alt={"carnegie_mellon_university Icon"}
 						className={"max-w-none"}
@@ -124,24 +131,24 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/download.png"}
 						alt={"Chinese Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/expedia.svg"}
 						alt={"expedia Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/firmenich.svg"}
 						alt={"firmenich Icon"}
 						className={"max-w-none"}
@@ -152,8 +159,8 @@ const SponserSection = () => {
 			<div className={"flex space-x-8 md:space-x-16 overflow-hidden"}>
 				<div className={"flex space-x-8 md:space-x-16 animate-loop-scroll"}>
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/glaxosmithkline.svg"}
 						alt={"glaxosmithkline Icon"}
 						className={"max-w-none"}
@@ -161,32 +168,32 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/merck_and_co.svg"}
 						alt={"merck_and_co Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/novo_nordisk.svg"}
 						alt={"novo_nordisk Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/pfizer.svg"}
 						alt={"pfizer Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/sri_international.svg"}
 						alt={"sri_international Icon"}
 						className={"max-w-none"}
@@ -194,34 +201,34 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/stanford_logo.svg"}
 						alt={"stanford_logo Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/the_university_of_wisconsin_madison.svg"}
 						alt={"the_university_of_wisconsin_madison Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/university_of_missouri.svg"}
 						alt={"university_of_missouri Icon"}
 						className={"max-w-none"}
 					/>
 				</div>
 
-				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll group-hover:paused"} aria-hidden="true">
+				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll"} aria-hidden="true">
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/glaxosmithkline.svg"}
 						alt={"glaxosmithkline Icon"}
 						className={"max-w-none"}
@@ -229,32 +236,32 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/merck_and_co.svg"}
 						alt={"merck_and_co Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/novo_nordisk.svg"}
 						alt={"novo_nordisk Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/pfizer.svg"}
 						alt={"pfizer Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/sri_international.svg"}
 						alt={"sri_international Icon"}
 						className={"max-w-none"}
@@ -262,31 +269,304 @@ const SponserSection = () => {
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/stanford_logo.svg"}
 						alt={"stanford_logo Icon"}
 						className={"max-w-none"}
 					/>
 					
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
 						src={"/sponsers/the_university_of_wisconsin_madison.svg"}
 						alt={"the_university_of_wisconsin_madison Icon"}
 						className={"max-w-none"}
 					/>
 
 					<Image
-						width={150}
-						height={150}
+						width={logo_width}
+						height={logo_height}
+						src={"/sponsers/university_of_missouri.svg"}
+						alt={"university_of_missouri Icon"}
+						className={"max-w-none"}
+					/>
+				</div>
+			</div>
+			</div>
+			
+			<div className={"block md:hidden flex space-x-8 md:space-x-16 overflow-hidden"}>
+				<div className={"flex space-x-8 md:space-x-16 animate-loop-scroll"}>
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/australian_national_university.svg"}
+						alt={"australian_national_university Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/aws.svg"}
+						alt={"AWS Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/bayer.svg"}
+						alt={"bayer Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/broad_inst.svg"}
+						alt={"broad_inst Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/carnegie_mellon_university.svg"}
+						alt={"carnegie_mellon_university Icon"}
+						className={"max-w-none"}
+
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/download.png"}
+						alt={"Chinese Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/expedia.svg"}
+						alt={"expedia Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/firmenich.svg"}
+						alt={"firmenich Icon"}
+						className={"max-w-none"}
+					/>
+				</div>
+
+				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll"} aria-hidden="true">
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/australian_national_university.svg"}
+						alt={"australian_national_university Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/aws.svg"}
+						alt={"AWS Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/bayer.svg"}
+						alt={"bayer Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/broad_inst.svg"}
+						alt={"broad_inst Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/carnegie_mellon_university.svg"}
+						alt={"carnegie_mellon_university Icon"}
+						className={"max-w-none"}
+
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/download.png"}
+						alt={"Chinese Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/expedia.svg"}
+						alt={"expedia Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/firmenich.svg"}
+						alt={"firmenich Icon"}
+						className={"max-w-none"}
+					/>
+				</div>
+			</div>
+
+			<div className={"block md:hidden flex space-x-8 md:space-x-16 overflow-hidden"}>
+				<div className={"flex space-x-8 md:space-x-16 animate-loop-scroll"}>
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/glaxosmithkline.svg"}
+						alt={"glaxosmithkline Icon"}
+						className={"max-w-none"}
+
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/merck_and_co.svg"}
+						alt={"merck_and_co Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/novo_nordisk.svg"}
+						alt={"novo_nordisk Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/pfizer.svg"}
+						alt={"pfizer Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/sri_international.svg"}
+						alt={"sri_international Icon"}
+						className={"max-w-none"}
+
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/stanford_logo.svg"}
+						alt={"stanford_logo Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/the_university_of_wisconsin_madison.svg"}
+						alt={"the_university_of_wisconsin_madison Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
 						src={"/sponsers/university_of_missouri.svg"}
 						alt={"university_of_missouri Icon"}
 						className={"max-w-none"}
 					/>
 				</div>
 
+				<div className={"bg-white flex space-x-8 md:space-x-16 animate-loop-scroll"} aria-hidden="true">
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/glaxosmithkline.svg"}
+						alt={"glaxosmithkline Icon"}
+						className={"max-w-none"}
 
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/merck_and_co.svg"}
+						alt={"merck_and_co Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/novo_nordisk.svg"}
+						alt={"novo_nordisk Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/pfizer.svg"}
+						alt={"pfizer Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/sri_international.svg"}
+						alt={"sri_international Icon"}
+						className={"max-w-none"}
+
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/stanford_logo.svg"}
+						alt={"stanford_logo Icon"}
+						className={"max-w-none"}
+					/>
+					
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/the_university_of_wisconsin_madison.svg"}
+						alt={"the_university_of_wisconsin_madison Icon"}
+						className={"max-w-none"}
+					/>
+
+					<Image
+						width={logo_width_mobile}
+						height={logo_height_mobile}
+						src={"/sponsers/university_of_missouri.svg"}
+						alt={"university_of_missouri Icon"}
+						className={"max-w-none"}
+					/>
+				</div>
 			</div>
 		</div>
 	)
